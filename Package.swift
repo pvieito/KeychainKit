760fefae93551715dcd46c7b577d88a284b731c7
Pedro José Pereira Vieito <pvieito@gmail.com>
2021-07-08 23:40:21 +0200

    [Infrastructure] Updated package

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "git@github.com:pvieito/FoundationKit.git", .branch("master")),
         .package(url: "git@github.com:pvieito/LoggerKit.git", .branch("master")),
         .package(url: "git@github.com:pvieito/CodeSignKit.git", .branch("master")),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
     ],
     targets: [
         .target(
